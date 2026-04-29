ce9314d86ff684ecd1bd69729c28b35a3e96d102
sutry09 <25MaksymSmorczewski.acc@lwetb.ie>
2026-04-29 15:36:09 +0100

    Add files via upload

---
+++ program 1.py
@@ -0,0 +1,8 @@
+students = int(input('how many students are there: '))
+coach = 550
+entryticket = 30
+tc = 550 + 30 * 15
+rtc = tc - 30
+cost_per_student = rtc
+cost_per_studend1 = rtc / students
+print(cost_per_student)
