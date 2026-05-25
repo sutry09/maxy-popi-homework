d7c2e7b7f300e12859dde6d43081795bde072f33
mrdilleen <mrdilleen@protonmail.com>
2026-05-25 12:43:01 +0100

    Feedback program 8.2.py

--- program 8.2.py
+++ program 8.2.py
@@ -1,3 +1,6 @@
+# Feedback - Mr. Dilleen
+#Great work, for the print statement at the end you do not need to put area in brackets (area)
+#all else is good.
 width = float(input('Enter the width of the square: '))
 length = float(input('Enter the length of the square: '))
 area = length * width 
