7004cc3f23aab93d4fd954ea1a9a06c59d1accfb
mrdilleen <mrdilleen@protonmail.com>
2026-05-25 12:26:37 +0100

    feedback program 1.py

--- program 1.py
+++ program 1.py
@@ -1,3 +1,7 @@
+#Feedback - Mr Dilleen
+#You have the right idea here but are missing some of the detail in the question. 
+#We will come back to these a lot next September so as long as you know how to carry out the
+#basic operations - add, subtract, input()... you will get better at these with practice
 students = int(input('how many students are there: '))
 coach = 550
 entryticket = 30
