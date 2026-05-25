92fa09d3b2076c5ed2ac0b49fbaf4ebeeaa389d0
mrdilleen <mrdilleen@protonmail.com>
2026-05-25 12:41:25 +0100

    Update program 8.1.py

--- program 8.1.py
+++ program 8.1.py
@@ -1,3 +1,5 @@
+#Feedback - Mr. Dilleen
+#Great work
 one_number = int(input('Enter the first number: '))
 two_number = int(input('Enter the second number: '))
 print('sum of two numbers: ' , one_number + two_number)
